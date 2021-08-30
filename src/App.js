5c7a8f16bf50f59b0920f73ade871b25b25169df
Mohamed Anes MOSTEFAOUI <m.anas@outlook.com>
2021-08-30 18:20:53 +0300

    update test

--- src/App.js
+++ src/App.js
@@ -4,12 +4,8 @@ import './App.css';
 function App() {
   return (
     <main>
-       <div class="wysiwyg wysiwyg--all-content css-1mw1dwe"><p>
-       المؤِّول
-
-
-       </p>
-       </div>
+<div class="wysiwyg wysiwyg--all-content css-1mw1dwe"><p>كثيرون يحمّلون النهضة مسؤولية ما يحدث اليوم بسبب عدم إرسائكم المحكمة الدستورية طوال السنوات الماضية ليتمكن الرئيس بعدها من استغلال هذا الفراغ القانوني ويصبح المؤِّول الوحيد للدستور.. ما رأيكم؟</p>
+</div>
     </main>
   );
 }
