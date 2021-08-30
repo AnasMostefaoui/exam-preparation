e5d0e1492d66a06447a8edb47f805599274ba01d
Mohamed Anes MOSTEFAOUI <m.anas@outlook.com>
2021-08-30 18:12:44 +0300

    Limit characters

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 function App() {
   return (
     <main>
-       <div class="wysiwyg wysiwyg--all-content css-1mw1dwe"><p>كثيرون يحمّلون النهضة مسؤولية ما يحدث اليوم بسبب عدم إرسائكم المحكمة الدستورية طوال السنوات الماضية ليتمكن الرئيس بعدها من استغلال هذا الفراغ القانوني ويصبح المؤِّول الوحيد للدستور.. ما رأيكم؟</p>
+       <div class="wysiwyg wysiwyg--all-content css-1mw1dwe"><p>
+       ؤِّ
+       {/* المؤِّول */}
+
+
+       </p>
        </div>
     </main>
   );
